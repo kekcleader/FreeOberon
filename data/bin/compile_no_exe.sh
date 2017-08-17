1a1a66e15f0efe000036356b2d587358ef2fd2b9
Artur Efimov <arturefimov@gmail.com>
2017-08-17 16:46:53 +0300

    Autodefines if module Graph is used (only directly)

--- data/bin/compile_no_exe.sh
+++ data/bin/compile_no_exe.sh
@@ -2,18 +2,11 @@
 # This script is run by FreeOberon on Linux.
 # Current directory of the script will be where
 # FreeOberon executable is located.
-# This particular script is for console programs.
+# This particular script is for compilation with no executable.
 cd bin
 VOCDIR=../data/bin/voc
 VOC=$VOCDIR/bin/voc
-CC=gcc
 $VOC -OC -cfFm ../Programs/$1
 retcode=$?
-if [ "$retcode" -eq "0" ]; then
-  THENAME="${1%.*}"
-  $CC -fPIC -g -I $VOCDIR/C/include \
-    -o $THENAME $THENAME.o \
-    $VOCDIR/lib/libvoc-OC.a
-fi
 cd ..
 exit $retcode
