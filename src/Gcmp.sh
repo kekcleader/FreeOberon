446ec3d2397400ee9feac2a19da26314f019e3a1
Arthur Yefimov <arturefimov@gmail.com>
2021-10-09 15:43:48 +0300

    Module Graph2 in progress

---
+++ src/Gcmp.sh
@@ -0,0 +1,13 @@
+ofront+ -887ws Allegro5.Mod && \
+ofront+ -887ws Graph2.Mod && \
+ofront+ -887wm GTest.Mod && \
+gcc -o GTest -g3 -O0 -fno-exceptions \
+  -I ../data/bin/OfrontPlus/Target/Linux_amd64/../../Mod/Lib \
+  -I ../data/bin/OfrontPlus/Target/Linux_amd64/Lib/Obj \
+  Graph2.c GTest.c \
+  ../data/bin/libFreeOberon.a \
+  ../data/bin/OfrontPlus/Target/Linux_amd64/Lib/libOfront.a \
+  $(pkg-config \
+    allegro_primitives-5 allegro_image-5 allegro_audio-5 \
+    allegro_acodec-5 allegro_font-5 allegro_dialog-5 \
+    allegro-5 --libs --cflags)
