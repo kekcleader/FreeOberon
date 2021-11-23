e6d3f187d72246f7a31c616edd367a66c9b67518
Arthur Yefimov <arturefimov@gmail.com>
2021-11-23 23:57:05 +0200

    Example of TermSnow

---
+++ src/compile_TermSnow.sh
@@ -0,0 +1,18 @@
+OFRDIR="../data/bin/OfrontPlus/Target/Linux_amd64"
+PATH="$OFRDIR:$PATH"
+export OBERON=.:$OFRDIR/Lib/Sym
+
+ofront+ -887ws Allegro5.Mod && \
+ofront+ -887ws Graph2.Mod && \
+ofront+ -887ws TermBox.Mod && \
+ofront+ -887wm TermSnow.Mod && \
+gcc -o TermSnow -g3 -O0 -fno-exceptions \
+  -I ../data/bin/OfrontPlus/Target/Linux_amd64/../../Mod/Lib \
+  -I ../data/bin/OfrontPlus/Target/Linux_amd64/Lib/Obj \
+  Graph2.c TermBox.c TermSnow.c \
+  ../data/bin/libFreeOberon.a \
+  ../data/bin/OfrontPlus/Target/Linux_amd64/Lib/libOfront.a \
+  $(pkg-config \
+    allegro_primitives-5 allegro_image-5 allegro_audio-5 \
+    allegro_acodec-5 allegro_font-5 allegro_dialog-5 \
+    allegro-5 --libs --cflags)
