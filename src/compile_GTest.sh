851025c1c31909dc92a53526854a00d82a8db005
Arthur Yefimov <arturefimov@gmail.com>
2022-01-16 05:47:21 +0200

    Examples fix

--- src/compile_GTest.sh
+++ src/compile_GTest.sh
@@ -1,12 +1,18 @@
-ofront+ -887ws Allegro5.Mod && \
-ofront+ -887ws Graph2.Mod && \
-ofront+ -887wm GTest.Mod && \
-gcc -o GTest -g3 -O0 -fno-exceptions \
-  -I ../Data/bin/OfrontPlus/Target/Linux_amd64/../../Mod/Lib \
-  -I ../Data/bin/OfrontPlus/Target/Linux_amd64/Lib/Obj \
-  Graph2.c GTest.c \
+PROG=GTest
+OFRDIR="../Data/bin/OfrontPlus/Target/Linux_amd64"
+PATH="$OFRDIR:$PATH"
+export OBERON=.:$OFRDIR/Lib/Sym
+OFR="ofront+ -s88"
+
+$OFR -7w Allegro5.Mod && \
+$OFR -7w Graph2.Mod && \
+$OFR -7wm $PROG.Mod && \
+gcc -o $PROG -g3 -O0 -fno-exceptions \
+  -I $OFRDIR/../../Mod/Lib \
+  -I $OFRDIR/Lib/Obj \
+  Graph2.c $PROG.c \
   ../Data/bin/libFreeOberon.a \
-  ../Data/bin/OfrontPlus/Target/Linux_amd64/Lib/libOfront.a \
+  $OFRDIR/Lib/libOfront.a \
   $(pkg-config \
     allegro_primitives-5 allegro_image-5 allegro_audio-5 \
     allegro_acodec-5 allegro_font-5 allegro_dialog-5 \
--- src/compile_TermSnow.sh
+++ src/compile_TermSnow.sh
@@ -1,17 +1,20 @@
+PROG=TermSnow
 OFRDIR="../Data/bin/OfrontPlus/Target/Linux_amd64"
 PATH="$OFRDIR:$PATH"
 export OBERON=.:$OFRDIR/Lib/Sym
+OFR="ofront+ -s88"
 
-ofront+ -887ws Allegro5.Mod && \
-ofront+ -887ws Graph2.Mod && \
-ofront+ -887ws TermBox.Mod && \
-ofront+ -887wm TermSnow.Mod && \
-gcc -o TermSnow -g3 -O0 -fno-exceptions \
-  -I ../Data/bin/OfrontPlus/Target/Linux_amd64/../../Mod/Lib \
-  -I ../Data/bin/OfrontPlus/Target/Linux_amd64/Lib/Obj \
-  Graph2.c TermBox.c TermSnow.c \
+$OFR -7w Allegro5.Mod && \
+$OFR -7w Graph2.Mod && \
+$OFR -7w TermBox.Mod && \
+$OFR -7w TermSnow.Mod && \
+$OFR -7wm $PROG.Mod && \
+gcc -o $PROG -g3 -O0 -fno-exceptions \
+  -I $OFRDIR/../../Mod/Lib \
+  -I $OFRDIR/Lib/Obj \
+  Graph2.c TermBox.c $PROG.c \
   ../Data/bin/libFreeOberon.a \
-  ../Data/bin/OfrontPlus/Target/Linux_amd64/Lib/libOfront.a \
+  $OFRDIR/Lib/libOfront.a \
   $(pkg-config \
     allegro_primitives-5 allegro_image-5 allegro_audio-5 \
     allegro_acodec-5 allegro_font-5 allegro_dialog-5 \
