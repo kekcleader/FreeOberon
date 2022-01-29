c6dea5e382f8da04926e928489bb25c982a317fd
Arthur Yefimov <arturefimov@gmail.com>
2022-01-30 01:53:05 +0200

    OpenScanner as in PO2013

--- src/edit.sh
+++ src/edit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-vim -p FreeOberon.Mod FoStrings.Mod ../Data/Texts/ru.dat Texts.Mod OV.Mod TermBox.Mod Graph2.Mod Allegro5.Mod
+vim -p FreeOberon.Mod FoStrings.Mod ../Data/Texts/ru.dat Texts.Mod OV.Mod
