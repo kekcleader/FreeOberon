8a34d7d91dd55e3adc33fce41be5bc1c18dfb50e
Arthur Yefimov <arturefimov@gmail.com>
2022-02-12 18:12:39 +0200

    Linux install.sh updated

--- install.sh
+++ install.sh
@@ -4,20 +4,25 @@ echo "This script will install Free Oberon."
 
 echo "Installing Allegro5, Git and GCC..."
 sudo apt-get update
-sudo apt-get install -y liballegro5-dev git gcc build-essential
+sudo apt-get install -y git gcc libc-dev liballegro5-dev
 
-echo "Installing Ofront+ as Git submodule..."
-git submodule update --init
+echo "======================="
+echo "Downloading Ofront+ if not exists..."
+# Only run git command if dot-git subdirectory exists
+[ -d ".git" ] && git submodule update --init
 
+echo "======================="
 echo "Building Ofront+..."
 cd Data/bin/OfrontPlus/Target/Linux_amd64/Bin
 ./build
 cd ../../../../../..
 
+echo "======================="
 echo "Building Free Oberon..."
 cd src
 ./make.sh
 cd ..
 
+echo "======================="
 echo "Done."
 echo "If everything went ok, you can now run: ./FreeOberon"
