5acf885b5e15a0a0f3e45d086e90677127fa8bc1
Arthur Yefimov <arturefimov@gmail.com>
2022-02-17 21:08:04 +0200

    Out.Real: now works

--- src/edit.sh
+++ src/edit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-vim -p FreeOberon.Mod Graph.Mod Allegro5.Mod ../Data/bin/*.sh
+vim -p Out.Mod
 #vim -p Texts.Mod Files.Mod FreeOberon.Mod FoStrings.Mod ../Data/Texts/ru.dat Texts.Mod OV.Mod
