2ae044a9c4d42f1e88c91c8870127b83cf5f052e
Arthur Yefimov <arturefimov@gmail.com>
2022-02-18 02:34:08 +0200

    Reals: Fix issue with Out.RealFix(9.999999, 0, 6) being 1

--- src/edit.sh
+++ src/edit.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-vim -p Out.Mod
-#vim -p Texts.Mod Files.Mod FreeOberon.Mod FoStrings.Mod ../Data/Texts/ru.dat Texts.Mod OV.Mod
+vim -p FreeOberon.Mod Editor.Mod FoStrings.Mod ../Data/Texts/ru.dat
