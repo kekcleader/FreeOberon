78e99570f81cd934a82556f54339343eed68d4e6
Arthur Yefimov <arturefimov@gmail.com>
2022-04-23 17:00:47 +0300

    Pack Linux script fix

--- src/pack_linux.sh
+++ src/pack_linux.sh
@@ -24,9 +24,9 @@ echo ================================
 echo      BEGIN PACK
 
 [ -d "$FOLDER" ] && mv $FOLDER $BAKFOLDER
-mkdir -p $FOLDER/bin
-cp ../bin/fungame.in $FOLDER/bin/
-cp ../bin/TEXT.DAT $FOLDER/bin/
+mkdir -p $FOLDER/_Build
+cp ../_Build/fungame.in $FOLDER/_Build/
+cp ../_Build/TEXT.DAT $FOLDER/_Build/
 
 cp ../*.{md,sh} $FOLDER/
 cp ../LICENSE $FOLDER/
